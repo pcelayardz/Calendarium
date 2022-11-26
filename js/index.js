c50ea822a1b578eb957ceb8b2c3609e4982119a9
Perla Celaya <pcelayardz@gmail.com>
2022-11-26 23:05:46 +0100

    Añadido funcion de form para eventos, tareas y cumpleaños, falta añadir al calendario

--- js/index.js
+++ js/index.js
@@ -128,33 +128,123 @@ function actionToggle() {
 
 function showFormCalendar() {
     let x = document.getElementById("formCalendario");
-    
+    let main = document.getElementById("principal")
+    let header = document.getElementById("header")
+    let navegacion = document.getElementById("navegacion")
     if (x.style.display === "none") {
         x.style.display = "block";
+        main.setAttribute("class","blur")
+        header.setAttribute("class","blur")
+        navegacion.setAttribute("class","blur")
     } else {
         x.style.display = "none";
+        main.setAttribute("class",null)
+        header.setAttribute("class",null)
+        navegacion.setAttribute("class",null)
     }
 }
 
+let evento = false;
+let tarea = false;
+let cumple = false;
+
 function formEvento(){
-    let x = document.getElementById("formCalendario");
-    let inputText = document.createElement("input");
-    inputText.setAttribute("type","text")
-    inputText.setAttribute("placeholder","Nombre evento")
-    inputText.setAttribute("id","nombreEvento")
-    let inputHora = document.createElement("input");
-    inputHora.setAttribute("type","datetime")
-    inputHora.setAttribute("placeholder","Hora evento")
-    inputHora.setAttribute("id","horaEvento")
-    let inputFecha = document.createElement("input")
-    inputFecha.setAttribute("type","date")
-    inputFecha.setAttribute("id","fechaEvento")
-    let inputEnviar = document.createElement("input")
-    inputEnviar.setAttribute("value","Enviar evento")
-    inputEnviar.setAttribute("type","button")
-    inputEnviar.setAttribute("id","enviarEvento")
-    x.appendChild(inputText);
-    x.appendChild(inputHora)
-    x.appendChild(inputFecha)
-    x.appendChild(inputEnviar)
+    if(tarea == true || cumple == true){
+        eliminarTarea()
+        eliminarCumple()
+    }
+    evento = !evento;
+    tarea=false;
+    cumple=false;
+    if(evento == true && tarea == false && cumple == false){
+        let x = document.getElementById("formCalendario");
+        let inputText = document.createElement("input");
+        inputText.setAttribute("type","text")
+        inputText.setAttribute("placeholder","Nombre evento")
+        inputText.setAttribute("id","nombreEvento")
+        let inputHora = document.createElement("input");
+        inputHora.setAttribute("type","datetime")
+        inputHora.setAttribute("placeholder","Hora evento")
+        inputHora.setAttribute("id","horaEvento")
+        let inputFecha = document.createElement("input")
+        inputFecha.setAttribute("type","date")
+        inputFecha.setAttribute("id","fechaEvento")
+        let inputEnviar = document.createElement("input")
+        inputEnviar.setAttribute("value","Enviar evento")
+        inputEnviar.setAttribute("type","button")
+        inputEnviar.setAttribute("id","enviarEvento")
+        x.appendChild(inputText);
+        x.appendChild(inputHora)
+        x.appendChild(inputFecha)
+        x.appendChild(inputEnviar)
+    }else{
+        eliminarEvento()
+    }
+    
+}
+function formTarea(){
+    if(evento == true || cumple == true){
+        eliminarEvento();
+        eliminarCumple();
+    }
+    tarea = !tarea;
+    evento=false;
+    cumple=false;
+    if(tarea == true && evento == false && cumple == false){
+        let x = document.getElementById("formCalendario");
+        let inputText = document.createElement("input");
+        inputText.setAttribute("type","text");
+        inputText.setAttribute("placeholder","Nombre Tarea");
+        inputText.setAttribute("id","nombreTarea");
+        let inputEnviar = document.createElement("input");
+        inputEnviar.setAttribute("value","Enviar tarea");
+        inputEnviar.setAttribute("type","button");
+        inputEnviar.setAttribute("id","enviarTarea");
+        x.appendChild(inputText);
+        x.appendChild(inputEnviar);
+    }else{
+        eliminarTarea();
+    }
+    
+}
+function formCumple(){
+    if(evento == true || tarea == true){
+        eliminarEvento();
+        eliminarTarea();
+    }
+    cumple = !cumple;
+    evento=false;
+    tarea=false;
+    if(cumple == true && evento == false && tarea == false){
+        let x = document.getElementById("formCalendario");
+        let inputText = document.createElement("input");
+        inputText.setAttribute("type","text");
+        inputText.setAttribute("placeholder","Nombre Cumpleañero");
+        inputText.setAttribute("id","nombreCumple");
+        let inputEnviar = document.createElement("input");
+        inputEnviar.setAttribute("value","Enviar cumpleaños");
+        inputEnviar.setAttribute("type","button");
+        inputEnviar.setAttribute("id","enviarCumple");
+        x.appendChild(inputText);
+        x.appendChild(inputEnviar);
+    }else{
+        eliminarCumple()
+    }
+    
+}
+
+function eliminarEvento(){
+    document.getElementById("nombreEvento").remove();
+    document.getElementById("horaEvento").remove();
+    document.getElementById("fechaEvento").remove();
+    document.getElementById("enviarEvento").remove();
+}
+
+function eliminarTarea(){
+    document.getElementById("enviarTarea").remove();
+    document.getElementById("nombreTarea").remove();
+}
+function eliminarCumple(){
+    document.getElementById("enviarCumple").remove();
+    document.getElementById("nombreCumple").remove();
 }
